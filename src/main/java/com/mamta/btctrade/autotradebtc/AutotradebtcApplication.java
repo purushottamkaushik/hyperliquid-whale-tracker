6c62f25db5eeea48b7f287d59cba53dbb7fe1ae1
dhriti <purushottamkaushik96@gmail.com>
2026-08-31 20:34:22 +0530

    first commit

---
+++ src/main/java/com/mamta/btctrade/autotradebtc/AutotradebtcApplication.java
@@ -0,0 +1,13 @@
+package com.mamta.btctrade.autotradebtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotradebtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutotradebtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mamta/btctrade/autotradebtc/AutotradebtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamta.btctrade.autotradebtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotradebtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
